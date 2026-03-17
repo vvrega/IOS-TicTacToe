54468d653e47c2a89b86b03bf51ce8a071115881
Marcin Wręga <120344089+vvrega@users.noreply.github.com>
2026-03-17 10:53:17 +0100

    init: initial project setup

---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Marcin Wręga on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
